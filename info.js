7614bd8156e0e5f64ef258a7e1c1550d33f3c045
icanwallet <96780182+godocms@users.noreply.github.com>
2021-12-30 10:14:19 +0800

    init

---
+++ info.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "key": "godo_layui",
+    "dir": __dirname,
+    "info": {
+        "author": "godocms",
+        "url": "https://godocms.com",
+        "email" : "4294979@qq.com"
+    },
+    "url": "layui",
+    "type": "ui",
+    "name": "layui"
+}
